5cfdaf5556f38cfab62b4dfb659ecab8a618dd3e
quentin <quentin@minster.io>
2018-06-01 00:41:54 +0200

    variables: new project

---
+++ variables/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let x: u128 = 0x1337u128;
+    let b = b'\x20';
+    let s = "😻";
+
+    println!("{}{}", b, s);
+
+    let mut t: (u128, &str, bool, f32) = (x, "foo", true, 3.14);
+    let t0 = t.0;
+    let t1 = t.1;
+    let t2 = t.2;
+    t.2 = false;
+    let t3 = t.3;
+
+    println!("{} {} {} {}", t0, t1, t2, t3);
+
+    let a = [1, 2, 3];
+
+    println!("{}", a[5]);
+}
