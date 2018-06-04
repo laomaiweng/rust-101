f52cc376d533d04e7970e2d574081b4664cfabb2
quentin <quentin@minster.io>
2018-06-04 15:59:52 +0200

    integer_stats: new project

---
+++ integer_stats/src/main.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+use std::env;
+
+fn main() {
+    // gather arguments as integers
+    let ints: Vec<i128> = env::args().skip(1).map(|s| s.parse().expect(&format!("not an int: {}", s))).collect();
+    assert!(! ints.is_empty(), "no integers");
+
+    println!("mean: {}", mean(&ints));
+    println!("median: {}", median(&ints));
+    println!("mode: {:?}", mode(&ints));
+}
+
+fn mean(ints: &Vec<i128>) -> f64 {
+    let sum: i128 = ints.iter().sum();
+    (sum as f64) / (ints.len() as f64)
+}
+
+fn median(ints: &Vec<i128>) -> i128 {
+    let mut ints = ints.clone();
+    ints.sort();
+
+    let imed = if ints.len() % 2 == 0 {
+        ints.len() / 2 - 1
+    } else {
+        ints.len() / 2
+    };
+
+    ints[imed]
+}
+
+fn mode(ints: &Vec<i128>) -> Vec<i128> {
+    let mut ints = ints.clone();
+    ints.sort();
+
+    let mut count: HashMap<i128, u128> = HashMap::new();
+    for i in &ints {
+        *count.entry(*i).or_insert(0) += 1;
+    }
+
+    let mut rcount: HashMap<u128, Vec<i128>> = HashMap::new();
+    let mut max: u128 = 0;
+    for (i, c) in &count {
+        rcount.entry(*c).or_insert(Vec::new()).push(*i);
+        if *c > max {
+            max = *c;
+        }
+    }
+
+    rcount.get(&max).unwrap().clone()
+}
