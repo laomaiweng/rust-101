a12f071683066bbe62d647aadd05fdcb4b879922
quentin <quentin@minster.io>
2018-06-18 01:49:34 +0200

    lifetimes: add struct with lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,15 @@
+use std::fmt;
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> fmt::Display for ImportantExcerpt<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "`{}`", self.part)
+    }
+}
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -12,4 +24,10 @@ fn main() {
 
     let result = longest(&string1, string2);
     println!("The longest string is: {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next()
+        .expect("Could not find a '.'");
+    let excerpt = ImportantExcerpt{ part: first_sentence };
+    println!("{}", excerpt);
 }
