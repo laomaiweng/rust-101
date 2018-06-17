ee1e36980198c5fdadb11c9379e18772b9b63b39
quentin <quentin@minster.io>
2018-06-17 17:06:42 +0200

    generics: fix implementation with trait bounds

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,39 +1,79 @@
-#[derive(Debug)]
+#![feature(specialization)]
+
+extern crate num_traits;
+
+use std::cmp::Ordering;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
 struct Point<T> {
     x: T,
     y: T,
 }
 
-impl<T> Point<T> {
+impl<T> Point<T>
+{
     fn x(&self) -> &T {
         &self.x
     }
 
-    fn y(&self) -> T {
-        self.y
+    fn y(&self) -> &T {
+        &self.y
     }
 }
 
-impl Point<f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+trait Norm<T> {
+    fn norm(&self) -> T;
+}
+
+impl<T> Norm<T> for Point<T>
+    where T: num_traits::Num + Copy
+{
+    default fn norm(&self) -> T {
+        self.x * self.y
     }
 }
 
-impl Point<i32> {
-    fn distance_from_origin(&self) -> i32 {
+//TODO: breaks, why?
+//impl Norm<u32> for Point<u32> {
+//    fn norm(&self) -> u32 {
+//        self.x * self.y
+//    }
+//}
+
+impl<T> Norm<T> for Point<T>
+    where T: num_traits::Signed + Copy
+{
+    default fn norm(&self) -> T {
         self.x.abs() + self.y.abs()
     }
 }
 
-#[derive(Debug)]
+impl Norm<f32> for Point<f32> {
+    fn norm(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+impl<T> PartialOrd for Point<T>
+    where T: PartialOrd + num_traits::Num + Copy
+{
+    fn partial_cmp(&self, other: &Point<T>) -> Option<Ordering> {
+        self.norm().partial_cmp(&other.norm())
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 struct ConfusedPoint<T, U> {
     x: T,
     y: U,
 }
 
-impl<T, U> ConfusedPoint<T, U> {
-    fn mixup<V, W>(self, other: ConfusedPoint<V, W>) -> ConfusedPoint<T, W> {
+impl<T, U> ConfusedPoint<T, U>
+    where T: Copy
+{
+    fn mixup<V, W>(&self, other: &ConfusedPoint<V, W>) -> ConfusedPoint<T, W> 
+        where W: Copy
+    {
         ConfusedPoint {
             x: self.x,
             y: other.y,
@@ -41,7 +81,9 @@ impl<T, U> ConfusedPoint<T, U> {
     }
 }
 
-fn largest<T>(list: &[T]) -> T {
+fn largest<T>(list: &[T]) -> T 
+    where T: PartialOrd + Copy
+{
     let mut result = list[0];
     for &item in list.iter() {
         if item > result {
@@ -61,16 +103,20 @@ fn main() {
     let result = largest(&char_list);
     println!("The largest char is {}", result);
 
-    let ipt_list = vec![Point{ x: 5, y: 10 }, Point{ x:-3, y: 30 }, Point{ x:9, y:8 }];
+    let ipt_list = vec![Point{ x: 5, y: 10 }, Point{ x:-3, y: 20 }, Point{ x:9, y:8 }];
     let result = largest(&ipt_list);
     println!("The largest integer point is {:?}", result);
 
-    let fpt_list = vec![Point{ x: 5., y: 10. }, Point{ x:-3., y: 30. }, Point{ x:9., y:8. }];
+    let upt_list = vec![Point{ x: 5u32, y: 10u32 }, Point{ x:3u32, y: 20u32 }, Point{ x:9u32, y:8u32 }];
+    let result = largest(&upt_list);
+    println!("The largest unsigned integer point is {:?}", result);
+
+    let fpt_list = vec![Point{ x: 5., y: 10. }, Point{ x:-3., y: 20. }, Point{ x:9., y:8. }];
     let result = largest(&fpt_list);
     println!("The largest floating point point is {:?}", result);
 
     let p1 = ConfusedPoint{ x: 5, y: 10.4 };
     let p2 = ConfusedPoint{ x: "Hello", y: 'c' };
-    let p3 = p2.mixup(p1);
+    let p3 = p2.mixup(&p1);
     println!("mixup({:?}, {:?}) = {:?}", p1, p2, p3);
 }
