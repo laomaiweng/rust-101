92d497800926ca2c8df42480ef8e70ed049be1f0
quentin <quentin@minster.io>
2018-06-24 02:08:14 +0200

    add: new workspace

---
+++ add/adder/src/main.rs
@@ -0,0 +1,7 @@
+extern crate add_one;
+extern crate add_two;
+
+fn main() {
+    let num = 10;
+    println!("Hello, world! {} plus one is {}! {} plus two is {}!", num, add_one::add_one(num), num, add_two::add_two(num));
+}
